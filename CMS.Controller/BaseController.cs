ffbd8e0b8ecf09a0c4512b09ebe859ba3ce8d72d
Jeff <talione@outlook.com>
2017-12-28 10:26:10 +0800

    Update BaseController.cs

--- CMS.Controller/BaseController.cs
+++ CMS.Controller/BaseController.cs
@@ -42,6 +42,85 @@ namespace CMS.ControllerCollection
             result = result.Replace("\r", "\\r").Replace("\n", "\\n");
             return result;
         }
+        
+        protected string DataTableToJson(DataTable dt)  //比ToJson多加了TableName
+        {
+            StringBuilder jsonBuilder = new StringBuilder();
+            jsonBuilder.Append("{\"");
+            jsonBuilder.Append(dt.TableName);
+            jsonBuilder.Append("\",");
+            jsonBuilder.Append("[");
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                jsonBuilder.Append("{");
+                for (int j = 0; j < dt.Columns.Count; j++)
+                {
+                    jsonBuilder.Append("\"");
+                    jsonBuilder.Append(dt.Columns[j].ColumnName);
+                    jsonBuilder.Append("\":\"");
+                    jsonBuilder.Append(dt.Rows[i][j].ToString());
+                    jsonBuilder.Append("\",");
+                }
+                jsonBuilder.Remove(jsonBuilder.Length - 1, 1);
+                jsonBuilder.Append("},");                
+            }
+            jsonBuilder.Remove(jsonBuilder.Length - 1, 1);
+            jsonBuilder.Append("]");
+            jsonBuilder.Append("}");
+            return jsonBuilder.ToString();
+        }
+
+        protected string ToJson(DataTable dt)
+        {
+            JavaScriptSerializer json = new JavaScriptSerializer();
+            json.MaxJsonLength = Int32.MaxValue;
+            ArrayList aList = new ArrayList();
+            foreach (DataRow dr in dt.Rows)
+            {
+                Dictionary<string, object> dict = new Dictionary<string, object>();
+                foreach (DataColumn dc in dt.Columns)
+                {
+                    dict.Add(dc.ColumnName, dr[dc.ColumnName].ToString());                    
+                }
+                aList.Add(dict);
+            }
+            return json.Serialize(aList);
+        }
+
+        public DataTable ToDataTable(string json)
+        {
+            DataTable dt = new DataTable();
+            DataTable result;
+            JavaScriptSerializer jsonS = new JavaScriptSerializer();
+            jsonS.MaxJsonLength = Int32.MaxValue;
+            ArrayList aList = jsonS.Deserialize<ArrayList>(json);
+            if (aList.Count > 0)
+            {
+                foreach (Dictionary<string, object> dict in aList)
+                {
+                    if (dict.Keys.Count == 0)
+                    {
+                        result = dt;
+                        return result;
+                    }
+                    if (dt.Columns.Count == 0)
+                    {
+                        foreach (string current in dict.Keys)
+                        {
+                            dt.Columns.Add(current, dict[current].GetType());
+                        }
+                    }
+                    DataRow dr = dt.NewRow();
+                    foreach (string current in dict.Keys)
+                    {
+                        dr[current] = dict[current];
+                    }
+                    dt.Rows.Add(dr);
+                }
+            }
+            result = dt;
+            return result;
+        }
 
         /// <summary>
         /// 对前端主动通过encodeURIComponent加密的字符串进行解密
